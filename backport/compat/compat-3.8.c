88e5a8085d6f2a19b9d8ba5a973c7f2adfd8c781
Hauke Mehrtens <hauke@hauke-m.de>
2013-12-08 16:08:08 +0100

    backports: add missing include for linux/of.h
    Upstream commit: a9081420811be2a9c7386b4693c5cf1b93b3ae23
    
    This fixes a compile error with some config options.
    Closes bug: https://bugzilla.kernel.org/show_bug.cgi?id=66601
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
    Reported-by: Richard Röjfors <richard.rojfors@gmail.com>
    Signed-off-by: Luis R. Rodriguez <mcgrof@do-not-panic.com>

--- backport/compat/compat-3.8.c
+++ backport/compat/compat-3.8.c
@@ -16,6 +16,7 @@
 #include <linux/module.h>
 #include "hid-ids.h"
 #include <linux/netdevice.h>
+#include <linux/of.h>
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3,7,8))
 void netdev_set_default_ethtool_ops(struct net_device *dev,
