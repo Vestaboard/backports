8ccb0e28b9782d9f1cd515e8ed7f8067b4267010
Florian Grandel <fgrandel@gmail.com>
2015-06-26 20:14:13 +0200

    headers: add missing include statement
    Upstream commit: 84cf1166445ccb36af7d0071c06a6643cf0b6e45
    
    The net/route.h header is missing when compiling against the android msm
    kernel. Explicitly add it here to make the kernel compile.
    
    Signed-off-by: Florian Grandel <fgrandel@gmail.com>
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- backport/backport-include/net/ip6_fib.h
+++ backport/backport-include/net/ip6_fib.h
@@ -2,6 +2,7 @@
 #define __BACKPORT_NET_IP6_ROUTE_H
 #include_next <net/ip6_fib.h>
 #include <net/ip6_route.h>
+#include <linux/route.h>
 #include <linux/version.h>
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,10,0)
