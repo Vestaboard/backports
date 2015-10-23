b0bbdc0f6f6305e89d11e86d0298bda1834daa79
Johannes Berg <johannes.berg@intel.com>
2015-10-23 13:52:59 +0200

    backports: add ktime_get_seconds()
    Since commit 84b00607aeb8 ("mac80211: use ktime_get_seconds")
    mac80211 uses ktime_get_seconds(). This patch provide a backport
    using ktime_get_ts() for it.
    
    [commit message and time64_t taken from Arend's patch]
    
    Signed-off-by: Arend van Spriel <arend@broadcom.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

---
+++ backport/backport-include/linux/timekeeping.h
@@ -0,0 +1,21 @@
+#ifndef __BACKPORT_TIMKEEPING_H
+#define __BACKPORT_TIMKEEPING_H
+#include <linux/version.h>
+#include <linux/types.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,17,0)
+#include_next <linux/timekeeping.h>
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,19,0)
+static inline time64_t ktime_get_seconds(void)
+{
+	struct timespec t;
+
+	ktime_get_ts(&t);
+
+	return t.tv_sec;
+}
+#endif
+
+#endif /* __BACKPORT_TIMKEEPING_H */
