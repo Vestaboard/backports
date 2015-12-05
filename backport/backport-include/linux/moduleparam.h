4c9c83b888b134e412cd5d59852d5daea0c6bf2f
Hauke Mehrtens <hauke@hauke-m.de>
2015-12-05 14:43:51 +0100

    header: make __MODULE_INFO use __UNIQUE_ID
    Since commit 34182eea36 "moduleparam: use __UNIQUE_ID()" the
    __MODULE_INFO macro uses __UNIQUE_ID and not the line number to
    generate the constants.
    
    This is needed to prevent a build error in brcmfmac which now uses the
    BRCMF_FW_NVRAM_DEF macro which calls MODULE_FIRMWARE two times. The
    __LINE__ macro will return the same line for both calls and the
    compiler would generate the same global variable name two times with
    the same name in older kernel versions, which will make the build fail.
    Using __UNIQUE_ID() fixes this problem.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- backport/backport-include/linux/moduleparam.h
+++ backport/backport-include/linux/moduleparam.h
@@ -15,4 +15,18 @@ static inline void kernel_param_unlock(struct module *mod)
 }
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,8,0)
+#undef __MODULE_INFO
+#ifdef MODULE
+#define __MODULE_INFO(tag, name, info)					  \
+static const char __UNIQUE_ID(name)[]					  \
+  __used __attribute__((section(".modinfo"), unused, aligned(1)))	  \
+  = __stringify(tag) "=" info
+#else  /* !MODULE */
+/* This struct is here for syntactic coherency, it is not used */
+#define __MODULE_INFO(tag, name, info)					  \
+  struct __UNIQUE_ID(name) {}
+#endif
+#endif /* < 3.8 */
+
 #endif /* __BACKPORT_LINUX_MODULEPARAM_H */
