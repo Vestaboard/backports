0b5bbf2b09589a88c662279304882cc20a4083b5
Hauke Mehrtens <hauke@hauke-m.de>
2016-02-13 11:44:04 +0100

    backports: add phy_attached_info()
    This function was added to the mainline Linux kernel in commit
    2220943a2 "phy: Centralise print about attached phy".
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- backport/backport-include/linux/phy.h
+++ backport/backport-include/linux/phy.h
@@ -1,6 +1,7 @@
 #ifndef __BACKPORT_LINUX_PHY_H
 #define __BACKPORT_LINUX_PHY_H
 #include_next <linux/phy.h>
+#include <linux/compiler.h>
 #include <linux/version.h>
 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3,9,0))
@@ -20,6 +21,12 @@ static inline bool mdiobus_is_registered_device(struct mii_bus *bus, int addr)
 {
 	return bus->phy_map[addr];
 }
+
+#define phy_attached_print LINUX_BACKPORT(phy_attached_print)
+void phy_attached_print(struct phy_device *phydev, const char *fmt, ...)
+	__printf(2, 3);
+#define phy_attached_info LINUX_BACKPORT(phy_attached_info)
+void phy_attached_info(struct phy_device *phydev);
 #endif /* < 4.5 */
 
 #endif /* __BACKPORT_LINUX_PHY_H */
