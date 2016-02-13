56cbfe257dfefc924e7e4b35e428c87c9ccd2dd2
Hauke Mehrtens <hauke@hauke-m.de>
2016-02-13 13:11:11 +0100

    header: add phydev_name()
    phydev_name() was added to the mainline Linux kernel in commit
    84eff6d19 "phy: add phydev_name() wrapper".
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- backport/backport-include/linux/phy.h
+++ backport/backport-include/linux/phy.h
@@ -8,4 +8,12 @@
 	phy_connect(dev, bus_id, handler, 0, interface)
 #endif
 
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4,5,0))
+#define phydev_name LINUX_BACKPORT(phydev_name)
+static inline const char *phydev_name(const struct phy_device *phydev)
+{
+	return dev_name(&phydev->dev);
+}
+#endif /* < 4.5 */
+
 #endif /* __BACKPORT_LINUX_PHY_H */
