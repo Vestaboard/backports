8408ac947ef358f88431100a7dbd55c42482e5f4
Hauke Mehrtens <hauke@hauke-m.de>
2016-04-30 15:35:56 +0200

    backport: make media depend on kernel 3.8
    The media backports are not used by so many users in my opinion and to
    reduce the efforts needed to make the new changes to media introduced
    by new kernel versions work on older kernel versions just make it
    depend on kernel 3.8 and do not support all the older kernel versions
    for media any more. This makes it possible to remove some patches and
    other changes.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- backport/backport-include/linux/string.h
+++ backport/backport-include/linux/string.h
@@ -3,11 +3,6 @@
 #include_next <linux/string.h>
 #include <linux/version.h>
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,6,0))
-#define memweight LINUX_BACKPORT(memweight)
-extern size_t memweight(const void *ptr, size_t bytes);
-#endif
-
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(4,5,0))
 #define memdup_user_nul LINUX_BACKPORT(memdup_user_nul)
 extern void *memdup_user_nul(const void __user *, size_t);
