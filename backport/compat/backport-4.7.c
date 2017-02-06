db50e3954866fc1fc2f4a22ffecf2fa3ef2eaaca
Johannes Berg <johannes.berg@intel.com>
2017-02-06 17:17:39 +0100

    backports: include rhashtable only when needed
    This avoids including it when it doesn't exist, on older kernels.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- backport/compat/backport-4.7.c
+++ backport/compat/backport-4.7.c
@@ -11,13 +11,14 @@
 #include <linux/export.h>
 #include <linux/list.h>
 #include <linux/rcupdate.h>
-#include <linux/rhashtable.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/skbuff.h>
 #include <net/netlink.h>
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,1,0)
+#include <linux/rhashtable.h>
+
 /**
  * rhashtable_walk_init - Initialise an iterator
  * @ht:		Table to walk over
