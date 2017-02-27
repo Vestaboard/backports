Vestaboard/backports

commit 7d89b544a1d99ee074bb4a30fb0abc017c59ef5d

git-tracker: add an option to add a Change-Id to commits
When the resulting output trees are used with gerrit, it can be
useful for patches to have a Change-Id. Add an option to let the
script generate them.

Signed-off-by: Johannes Berg <johannes.berg@intel.com>
Signed-off-by: Johannes Berg <johannes.berg@intel.com>

devel/git-tracker.py
@@ -12,7 +12,7 @@ problem to occur (although this is less useful since lots of commits
 need to be squashed in the output tree.)
 """
 
-import sys, re, os, argparse, shutil
+import sys, re, os, argparse, shutil, hashlib
 try:
     import configparser as ConfigParser
 except:
@@ -56,7 +56,7 @@ def update_cache_objects(gittree, objdir, input):
 
 def handle_commit(args, msg, branch, treename, kernelobjdir, tmpdir, wgitdir, backport_rev, kernel_rev,
                   prev_kernel_rev=None, defconfig=None, env={}, commit_failure=True,
-                  append_shortlog=None):
+                  append_shortlog=None, add_changeid=False):
     log = []
     def logwrite(l):
         log.append(l)
@@ -127,6 +127,15 @@ def handle_commit(args, msg, branch, treename, kernelobjdir, tmpdir, wgitdir, ba
             'krev': kernel_rev,
           }
 
+            if add_changeid:
+                have_changeid = False
+                for line in msg.split('\n'):
+                    if line.lower().startswith('change-id: I'):
+                        have_changeid = True
+                        break
+                if not have_changeid:
+                    msg += 'Change-Id: I%s\n' % hashlib.sha1(msg).hexdigest()
+
             git.commit(msg, tree=wdir, env=env, opts=['-q', '--allow-empty'])
             git.push(opts=['-f', '-q', 'origin', branch], tree=wdir)
         os.rename(os.path.join(wdir, '.git'), wgitdir)
@@ -162,6 +171,10 @@ if __name__ == '__main__':
         if not config.has_option(tree, 'output'):
             print("No output defined in section %s" % tree)
             sys.exit(3)
+        if config.has_option(tree, 'changeid'):
+            if config.get(tree, 'changeid') != 'true':
+                print("changeid option can only take the value 'true'")
+                sys.exit(3)
 
     with tempdir.tempdir() as kernel_tmpdir:
         # get cachedir, or use temporary directory
@@ -182,6 +195,7 @@ if __name__ == '__main__':
             defconfig = None
             if config.has_option(tree, 'defconfig'):
                 defconfig = config.get(tree, 'defconfig')
+            add_changeid = config.has_option(tree, 'changeid')
             branches = [r.strip() for r in config.get(tree, 'branches').split(',')]
 
             update_cache_objects(input, kernelobjdir, input=True)
@@ -277,7 +291,8 @@ if __name__ == '__main__':
                                                 wgitdir, backport_rev, commit, env=env,
                                                 prev_kernel_rev=prev, defconfig=defconfig,
                                                 commit_failure=not catch_up_from_failure,
-                                                append_shortlog=append_shortlog)
+                                                append_shortlog=append_shortlog,
+                                                add_changeid=add_changeid)
                         if not failure:
                             prev = commit
                             catch_up_from_failure = False

commit b5ccf480b741e8e835360d20f7f3ede26dfad74e

git-tracker: add Change-Id to all types of commits if configured
My previous commit to optionally add a Change-Id missed two out
of three types of commits internally created by the tracker, add
the Change-Id to them as well.

Fixes: 7d89b544a1d9 ("git-tracker: add an option to add a Change-Id to commits")
Signed-off-by: Johannes Berg <johannes.berg@intel.com>

devel/git-tracker.py
@@ -239,7 +239,8 @@ if __name__ == '__main__':
                                       branch, tree, kernelobjdir,
                                       branch_tmpdir, wgitdir, backport_rev,
                                       kernel_head, defconfig=defconfig,
-                                      env=backport_author_env)
+                                      env=backport_author_env,
+                                      add_changeid=add_changeid)
                         continue
                     if old_data['backport'] == backport_rev and old_data[tree] == kernel_head:
                         continue
@@ -249,7 +250,8 @@ if __name__ == '__main__':
                                       branch, tree, kernelobjdir,
                                       branch_tmpdir, wgitdir, backport_rev,
                                       kernel_head, defconfig=defconfig,
-                                      env=backport_author_env)
+                                      env=backport_author_env,
+                                      add_changeid=add_changeid)
                         continue
                     # update from old to new
                     if last_success in old_data: