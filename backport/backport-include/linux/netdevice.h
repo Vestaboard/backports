Vestaboard/backports

commit 8cdd85766293018fea01512959320a05269e97b4

backports: add netdev_upper_dev_link() extack argument
But make it optional (using magic.h) to let this still work
on kernels that didn't change the argument.

Signed-off-by: Johannes Berg <johannes.berg@intel.com>

backport/backport-include/linux/netdevice.h
@@ -3,6 +3,7 @@
 #include_next <linux/netdevice.h>
 #include <linux/netdev_features.h>
 #include <linux/version.h>
+#include <backport/magic.h>
 
 /*
  * This is declared implicitly in newer kernels by netdevice.h using
@@ -331,6 +332,20 @@ static inline void netif_trans_update(struct net_device *dev)
 	(_dev)->priv_destructor = (_destructor);
 #define netdev_set_def_destructor(_dev) \
 	(_dev)->needs_free_netdev = true;
+#endif
+
+#if LINUX_VERSION_IS_LESS(4,14,0)
+static inline int _bp_netdev_upper_dev_link(struct net_device *dev,
+					    struct net_device *upper_dev)
+{
+	return netdev_upper_dev_link(dev, upper_dev);
+}
+#define netdev_upper_dev_link3(dev, upper, extack) \
+	netdev_upper_dev_link(dev, upper)
+#define netdev_upper_dev_link2(dev, upper) \
+	netdev_upper_dev_link(dev, upper)
+#define netdev_upper_dev_link(...) \
+	macro_dispatcher(netdev_upper_dev_link, __VA_ARGS__)(__VA_ARGS__)
 #endif
 
 #endif /* __BACKPORT_NETDEVICE_H */

commit 63bb0c5821ca4732f6e6db3d6fd606461a321c9e

header: netdevice.h: Fix netdev_upper_dev_link on 4.14
Commit 42ab19ee90292 ("net: Add extack to upper device linking") which
added an extra parameter to netdev_upper_dev_link() was added with
kernel 4.15 and was not backported to kernel 4.14, activate this
backport already for kernel 4.14.

Fixes: 8cdd85766293 ("backports: add netdev_upper_dev_link() extack argument")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
Signed-off-by: Johannes Berg <johannes.berg@intel.com>

backport/backport-include/linux/netdevice.h
@@ -334,7 +334,7 @@ static inline void netif_trans_update(struct net_device *dev)
 	(_dev)->needs_free_netdev = true;
 #endif
 
-#if LINUX_VERSION_IS_LESS(4,14,0)
+#if LINUX_VERSION_IS_LESS(4,15,0)
 static inline int _bp_netdev_upper_dev_link(struct net_device *dev,
 					    struct net_device *upper_dev)
 {