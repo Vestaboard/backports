Vestaboard/backports

commit 162a6b312f1b439cf0cf191564b21df0d3393564

add support for pci_alloc_irq_vectors
add support for function in kernels under 4.8
function is a wrapper around 'pci_enable_msi_block' or
'pci_enable_msi_range' depending on the kernel version
the min\max number of interrupts was overridden to always use the
max number of interrupts which seems correct since it was not
supported in the old APIs

Signed-off-by: Omer Dagan <omer.dagan@tandemg.com>
[use GPL expert as everything in backports, remove unused ifdef]
Signed-off-by: Johannes Berg <johannes.berg@intel.com>

backport/backport-include/linux/pci.h
@@ -134,6 +134,25 @@ static inline int pci_vfs_assigned(struct pci_dev *dev)
 
 #endif /* LINUX_VERSION_IS_LESS(3,10,0) */
 
+#if LINUX_VERSION_IS_LESS(4,8,0)
+#define pci_alloc_irq_vectors LINUX_BACKPORT(pci_alloc_irq_vectors)
+#ifdef CONFIG_PCI_MSI
+int pci_alloc_irq_vectors(struct pci_dev *dev, unsigned int min_vecs,
+		unsigned int max_vecs, unsigned int flags);
+#else
+static inline int pci_alloc_irq_vectors(struct pci_dev *dev, unsigned int min_vecs,
+		unsigned int max_vecs, unsigned int flags)
+{ return -ENOSYS; }
+#endif
+#endif
+
+#if LINUX_VERSION_IS_LESS(4,8,0)
+#define pci_free_irq_vectors LINUX_BACKPORT(pci_free_irq_vectors)
+static inline void pci_free_irq_vectors(struct pci_dev *dev)
+{
+}
+#endif
+
 #if LINUX_VERSION_IS_LESS(3,14,0)
 #define pci_enable_msi_range LINUX_BACKPORT(pci_enable_msi_range)
 #ifdef CONFIG_PCI_MSI

backport/compat/backport-4.8.c
@@ -9,6 +9,7 @@
  */
 #include <linux/usb.h>
 #include <linux/usb/cdc.h>
+#include <linux/pci.h>
 
 int cdc_parse_cdc_header(struct usb_cdc_parsed_header *hdr,
 			 struct usb_interface *intf,
@@ -144,3 +145,25 @@ next_desc:
 	return cnt;
 }
 EXPORT_SYMBOL_GPL(cdc_parse_cdc_header);
+
+int pci_alloc_irq_vectors(struct pci_dev *dev, unsigned int min_vecs,
+		unsigned int max_vecs, unsigned int flags)
+{
+	int res;
+	int msi_nvect = max_vecs;
+
+	if (max_vecs < min_vecs)
+		return -ERANGE;
+
+#if LINUX_VERSION_IS_LESS(3,15,0)
+	res = pci_enable_msi_block(dev, msi_nvect);
+	if (res == 0) {
+		return msi_nvect;
+	}
+#else
+	res = pci_enable_msi_range(dev, msi_nvect, msi_nvect);
+	return msi_nvect;
+#endif /*LINUX_VERSION_IS_LESS(3,15,0)*/
+	return -ENOSPC;
+}
+EXPORT_SYMBOL_GPL(pci_alloc_irq_vectors);

commit 1e4822d1394ab7e192eff168a0c1b3c7c4ca24ff

backports: Fix pci_alloc_irq_vectors() backport
This copies the pci_alloc_irq_vectors() function from kernel 4.9 and
replaces the __pci_enable_msi{x}_range() calls with calls to
pci_enable_msi{x}_range(), these were backported to kernel versions <
3.14, so no need to handle the older kernels specially here.

This also adds support for MSIx IRQs and adds the PCI_IRQ_* defines.

Fixes: 162a6b312f1b ("add support for pci_alloc_irq_vectors")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>
Cc: Omer Dagan <omer.dagan@tandemg.com>

backport/backport-include/linux/pci.h
@@ -224,4 +224,12 @@ static inline struct pci_dev *pcie_find_root_port(struct pci_dev *dev)
 
 #endif/* <4.9.0 but not >= 3.12.69, 4.4.37, 4.8.13 */
 
+#ifndef PCI_IRQ_LEGACY
+#define PCI_IRQ_LEGACY		(1 << 0) /* Allow legacy interrupts */
+#define PCI_IRQ_MSI		(1 << 1) /* Allow MSI interrupts */
+#define PCI_IRQ_MSIX		(1 << 2) /* Allow MSI-X interrupts */
+#define PCI_IRQ_ALL_TYPES \
+	(PCI_IRQ_LEGACY | PCI_IRQ_MSI | PCI_IRQ_MSIX)
+#endif
+
 #endif /* _BACKPORT_LINUX_PCI_H */

backport/compat/backport-4.8.c
@@ -148,25 +148,48 @@ EXPORT_SYMBOL_GPL(cdc_parse_cdc_header);
 
 #ifdef CONFIG_PCI
 #ifdef CONFIG_PCI_MSI
+
+/**
+ * pci_alloc_irq_vectors - allocate multiple IRQs for a device
+ * @dev:		PCI device to operate on
+ * @min_vecs:		minimum number of vectors required (must be >= 1)
+ * @max_vecs:		maximum (desired) number of vectors
+ * @flags:		flags or quirks for the allocation
+ *
+ * Allocate up to @max_vecs interrupt vectors for @dev, using MSI-X or MSI
+ * vectors if available, and fall back to a single legacy vector
+ * if neither is available.  Return the number of vectors allocated,
+ * (which might be smaller than @max_vecs) if successful, or a negative
+ * error code on error. If less than @min_vecs interrupt vectors are
+ * available for @dev the function will fail with -ENOSPC.
+ *
+ * To get the Linux IRQ number used for a vector that can be passed to
+ * request_irq() use the pci_irq_vector() helper.
+ */
 int pci_alloc_irq_vectors(struct pci_dev *dev, unsigned int min_vecs,
 		unsigned int max_vecs, unsigned int flags)
 {
-	int res;
-	int msi_nvect = max_vecs;
+	int vecs = -ENOSPC;
 
-	if (max_vecs < min_vecs)
-		return -ERANGE;
-
-#if LINUX_VERSION_IS_LESS(3,15,0)
-	res = pci_enable_msi_block(dev, msi_nvect);
-	if (res == 0) {
-		return msi_nvect;
+	if (flags & PCI_IRQ_MSIX) {
+		vecs = pci_enable_msix_range(dev, NULL, min_vecs, max_vecs);
+		if (vecs > 0)
+			return vecs;
 	}
-#else
-	res = pci_enable_msi_range(dev, msi_nvect, msi_nvect);
-	return msi_nvect;
-#endif /*LINUX_VERSION_IS_LESS(3,15,0)*/
-	return -ENOSPC;
+
+	if (flags & PCI_IRQ_MSI) {
+		vecs = pci_enable_msi_range(dev, min_vecs, max_vecs);
+		if (vecs > 0)
+			return vecs;
+	}
+
+	/* use legacy irq if allowed */
+	if ((flags & PCI_IRQ_LEGACY) && min_vecs == 1) {
+		pci_intx(dev, 1);
+		return 1;
+	}
+
+	return vecs;
 }
 EXPORT_SYMBOL_GPL(pci_alloc_irq_vectors);
 #endif /* CONFIG_PCI_MSI */