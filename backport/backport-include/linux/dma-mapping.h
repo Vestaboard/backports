06a4b36275d33e1e97e9f9cd659d98ed23bb98f1
Hauke Mehrtens <hauke@hauke-m.de>
2021-12-05 18:42:35 +0100

    headers: Add DMA_MAPPING_ERROR
    Upstream commit 9b9a5b573a295d6ae2844017b82a8bb8878fd12f
    
    DMA_MAPPING_ERROR definition was added to kernel 5.0 in commit
    42ee3cae0ed3 ("dma-mapping: provide a generic DMA_MAPPING_ERROR"). This
    is now used by ath10k.
    
    It should be fine to just add this definition, this is an invalid
    address and also in older kernel versions this is sued to mark an
    invalid address.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- backport/backport-include/linux/dma-mapping.h
+++ backport/backport-include/linux/dma-mapping.h
@@ -32,4 +32,18 @@ static inline int dma_set_mask_and_coherent(struct device *dev, u64 mask)
 }
 #endif /* LINUX_VERSION_IS_LESS(3,13,0) */
 
+#ifndef DMA_MAPPING_ERROR
+/*
+ * A dma_addr_t can hold any valid DMA or bus address for the platform.  It can
+ * be given to a device to use as a DMA source or target.  It is specific to a
+ * given device and there may be a translation between the CPU physical address
+ * space and the bus address space.
+ *
+ * DMA_MAPPING_ERROR is the magic error code if a mapping failed.  It should not
+ * be used directly in drivers, but checked for using dma_mapping_error()
+ * instead.
+ */
+#define DMA_MAPPING_ERROR		(~(dma_addr_t)0)
+#endif /* DMA_MAPPING_ERROR */
+
 #endif /* __BACKPORT_LINUX_DMA_MAPPING_H */
